7214cbfdccf01ccf846fe9ba8b9b563c46f8afda
stevefaeembra <stevendkay@gmail.com>
2023-03-08 10:34:19 +0000

    feat(frontend): Take out plugin visualiser
    Removed the rollup treemap plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,10 @@ import * as path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [react()],
   build: {
     target: 'ESNext',
   },
