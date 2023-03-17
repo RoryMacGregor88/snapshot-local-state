3bb7c23039d867c1b4ddaf24728e1a0ac40d6f9b
Rory MacGregor <rory.macgregor@astrosat.net>
2023-03-17 15:08:39 +0000

    feat(test): Create custom renderHook wrapper
    I created a custom renderHook wrapper very similar to the custom render
    wrapper. The purpose of this is to abstract out any providers or anything
    else that is required to wrap the hook being tested.

--- src/app/useAppConfig.test.tsx
+++ src/app/useAppConfig.test.tsx
@@ -1,30 +1,21 @@
-import { QueryClientProvider } from '@tanstack/react-query';
-import { renderHook } from '@testing-library/react';
 import { rest } from 'msw';
 import { expect, it } from 'vitest';
 
 import { server } from '~/mocks/server';
-import { waitFor } from '~/test/utils';
-import { generateClient } from '~/utils/generate-client';
+import { renderHook, waitFor } from '~/test/utils';
 
 import { useAppConfig } from './useAppConfig';
 
 describe('useAppConfig', () => {
-  it('fetches', async () => {
+  it('should fetch data to configure the frontend', async () => {
     const config = { name: 'Test Name' };
 
     server.use(rest.get('*/api/config', (req, res, ctx) => res(ctx.status(200), ctx.json(config))));
 
-    const wrapper = ({ children }) => {
-      return <QueryClientProvider client={generateClient()}>{children}</QueryClientProvider>;
-    };
-
-    const { result } = renderHook(() => useAppConfig(), { wrapper });
+    const { result } = renderHook(() => useAppConfig());
 
     await waitFor(() => result.current.isSuccess);
 
-    await waitFor(() => {
-      expect(result.current.data).toEqual(config);
-    });
+    await waitFor(() => expect(result.current.data).toEqual(config));
   });
 });
