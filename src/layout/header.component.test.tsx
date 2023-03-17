cb1c13e3ec97464ff36fa6ec108d9c5f625f7bc8
Mark Small <mark.small@astrosat.net>
2023-03-17 20:10:42 +0000

    fix(frontend): Fix error found by typescript compiler
    The header component takes a mandatory prop `appConfig`, but the test
    was not passing it, causing and error. I have now added the attribute
    to the header instance in the test.

--- src/layout/header.component.test.tsx
+++ src/layout/header.component.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { describe, expect, it } from 'vitest';
+import { AppConfig } from '~/app/useAppConfig';
 
 import { render, screen } from '~/test/utils';
 
@@ -8,7 +9,11 @@ import Header from './header.component';
 
 describe('Header', () => {
   it('should render the header', () => {
-    render(<Header />);
+    const config: AppConfig = {
+      name: 'Test Name',
+    };
+
+    render(<Header appConfig={config} />);
     expect(screen.getByRole('heading', { name: /hello user/i })).toBeInTheDocument();
   });
 });
