2bdcf933ab3912f68afb4518e045cafe58ad8c3e
Rory MacGregor <rory.macgregor@astrosat.net>
2023-05-18 12:17:21 +0100

    feat(frontend): Fix tests
    Husky won't let me push without fixing tests first.
    
    IssueID #

--- src/app/app.component.test.tsx
+++ src/app/app.component.test.tsx
@@ -2,11 +2,17 @@ import { describe, expect, it } from 'vitest';
 
 import { render, screen } from '~/test/utils';
 
+import { SnapshotProvider } from '~/snapshots/snapshot.context';
+
 import { App } from './app.component';
 
 describe('App', () => {
   it('should render the whole app', () => {
-    render(<App />);
+    render(
+      <SnapshotProvider>
+        <App />
+      </SnapshotProvider>,
+    );
 
     expect(screen.getByRole('heading', { name: /main content/i })).toBeInTheDocument();
   });
